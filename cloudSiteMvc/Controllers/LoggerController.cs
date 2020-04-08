96c792c8b13f74fed58e4262ba5051934480d22e
Max Osinov <osinov.maks@gmail.com>
2020-04-08 22:18:12 +0300

    upd

--- cloudSiteMvc/Controllers/LoggerController.cs
+++ cloudSiteMvc/Controllers/LoggerController.cs
@@ -3,16 +3,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using System.Web.UI;
+using cloudSiteMvc.Attributes;
 using cloudSiteMvc.Models;
 
 namespace cloudSiteMvc.Controllers
 {
+	[AuthorizationOrRedirect]
 	public class LoggerController : Controller
 	{
 		private readonly ApplicationDbContext _db = new ApplicationDbContext();
 
 
-		[OutputCache(Duration = 3600, Location = OutputCacheLocation.Client)]
+		[OutputCache(Duration = 1600, Location = OutputCacheLocation.Client)]
 		public async Task<ActionResult> LogInfo()
 		{
 			var loggers = await _db.Loggers.Include(l => l.User).OrderByDescending(i => i.DateTime).ToListAsync();
